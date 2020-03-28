1c48c3534295f8d375e821722d9a241bfb75145f
Yung Chun Lu <yungchunlu@Yungs-MacBook-Pro.local>
2020-03-28 21:18:00 +0700

    fixed review

--- upload_docker.sh
+++ upload_docker.sh
@@ -7,6 +7,8 @@
 dockerpath=780513/udacity_microservice:latest
 
 # Authenticate & tag
+docker login -u 780513 --password-stdin
+docker tag udacity_microservice $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Push image to a docker repository
